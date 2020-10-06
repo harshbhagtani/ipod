5d277f604f3d9681eccde89c7c18be4056566f5a
harsh <harsh.bhagtani1@gmail.com>
2020-10-06 20:13:12 +0530

    Updates

---
+++ precache-manifest.8956c3a5a14e17c75bac919b40a17cf8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e411ca95e80cd25c59e489c6d6f536df",
+    "url": "/ipod/index.html"
+  },
+  {
+    "revision": "74ac371faf44f43dd4f9",
+    "url": "/ipod/static/css/main.e0719f41.chunk.css"
+  },
+  {
+    "revision": "327198e54ac9621e4f08",
+    "url": "/ipod/static/js/2.b446eb98.chunk.js"
+  },
+  {
+    "revision": "3b6069e01f0db714f9e787b4dbe9775c",
+    "url": "/ipod/static/js/2.b446eb98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74ac371faf44f43dd4f9",
+    "url": "/ipod/static/js/main.861096a6.chunk.js"
+  },
+  {
+    "revision": "e6acf8b982710a9850e8",
+    "url": "/ipod/static/js/runtime-main.59161aa3.js"
+  }
+]);
